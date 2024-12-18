e7871c408b18d0533c07032428a255a0208ee1d7
Cristileny Alves de Jesus Machado <52088919805@>
2024-12-18 14:49:16 -0300

    Primeiro commit

---
+++ front/scripts/database.js
@@ -0,0 +1,145 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"ThunderStep Flash Runner", 
+        preco: 249.90, 
+        descricao:"Agilidade sem limites. Com design aerodinâmico e sola ultraleve, o Flash Runner é perfeito para quem quer velocidade e estilo em cada passo.",
+        imagemProduto: {
+            img1:"imagens produtos/a-completely-white-background-image-of-a-pair-of-m-3c7uSJWEQmGcb66qVhslsQ-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img2:"imagens produtos/a-completely-white-background-image-of-a-pair-of-m-3c7uSJWEQmGcb66qVhslsQ-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img3:"imagens produtos/a-completely-white-background-image-of-a-pair-of-m-3c7uSJWEQmGcb66qVhslsQ-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img4:"imagens produtos/a-completely-white-background-image-of-a-pair-of-m-3c7uSJWEQmGcb66qVhslsQ-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"ThunderStep Urban Pulse", 
+        preco: 299.90, 
+        descricao:"O ritmo das ruas em seus pés. Feito para quem vive a cidade intensamente, o Urban Pulse combina conforto extremo com um visual moderno e arrojado.",
+        imagemProduto: {
+            img1:"imagens produtos/a-completely-white-background-image-of-a-sneaker-w-zfvWOfpAQUqs73eTTT1KUQ-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img2:"imagens produtos/a-completely-white-background-image-of-a-sneaker-w-zfvWOfpAQUqs73eTTT1KUQ-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img3:"imagens produtos/a-completely-white-background-image-of-a-sneaker-w-zfvWOfpAQUqs73eTTT1KUQ-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img4:"imagens produtos/a-completely-white-background-image-of-a-sneaker-w-zfvWOfpAQUqs73eTTT1KUQ-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+    },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"ThunderStep Storm Trail", 
+        preco: 219.90, 
+        descricao:"Pronto para qualquer terreno. O Storm Trail traz aderência superior e resistência para encarar trilhas, aventuras e qualquer desafio que surgir no caminho.",
+        imagemProduto: {
+            img1:"imagens produtos/a-photo-of-a-pair-of-minimalist-sneakers-from-the--kafZTpSaR8iNDvZG0TDelQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img2:"imagens produtos/a-photo-of-a-pair-of-minimalist-sneakers-from-the--kafZTpSaR8iNDvZG0TDelQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img3:"imagens produtos/a-photo-of-a-pair-of-minimalist-sneakers-from-the--kafZTpSaR8iNDvZG0TDelQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img4:"imagens produtos/a-photo-of-a-pair-of-minimalist-sneakers-from-the--kafZTpSaR8iNDvZG0TDelQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
+        exibirHome: false
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"ThunderStep Sky Glide", 
+        preco: 399.90, 
+        descricao:"Leve como o vento. Com amortecimento avançado e design minimalista, o Sky Glide oferece conforto o dia todo sem abrir mão do estilo.",
+        imagemProduto: {
+            img1:"imagens produtos/a-photo-of-a-pair-of-thunderstep-sneakers-on-a-whi-U-qEZO8TT4aasSsqUmabJg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img2:"imagens produtos/a-photo-of-a-pair-of-thunderstep-sneakers-on-a-whi-U-qEZO8TT4aasSsqUmabJg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img3:"imagens produtos/a-photo-of-a-pair-of-thunderstep-sneakers-on-a-whi-U-qEZO8TT4aasSsqUmabJg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img4:"imagens produtos/a-photo-of-a-pair-of-thunderstep-sneakers-on-a-whi-U-qEZO8TT4aasSsqUmabJg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"ThunderStep Neon Blast", 
+        preco: 279.90, 
+        descricao:"Impacto em cada passada. O Thunderbolt une tecnologia de alta performance com um design marcante, ideal para quem gosta de treinar com intensidade e atitude.",
+        imagemProduto: {
+            img1:"./imagens produtos/a-pair-of-sneakers-with-a-bold-and-eye-catching-de-TB56nDQ8RgKmU5yCrp60hg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img2:"./imagens produtos/a-pair-of-sneakers-with-a-bold-and-eye-catching-de-TB56nDQ8RgKmU5yCrp60hg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img3:"./imagens produtos/a-pair-of-sneakers-with-a-bold-and-eye-catching-de-TB56nDQ8RgKmU5yCrp60hg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+            img4:"./imagens produtos/a-pair-of-sneakers-with-a-bold-and-eye-catching-de-TB56nDQ8RgKmU5yCrp60hg-n1nGw0jBTxu_1FGcFpwk2Q.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"ThunderStep Urban Flux", 
+        preco: 299.90, 
+        descricao:"Estilo e conforto para marcar presença nas ruas com o ThunderStep Urban Flux. Perfeito para quem busca se destacar no streetwear.",
+        imagemProduto: {
+            img1:"./imagens produtos/a-realistic-image-of-a-minimalistic-sneaker-with-a-ICsNURNFTS-vSKddI9G6CQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img2:"./imagens produtos/a-realistic-image-of-a-minimalistic-sneaker-with-a-ICsNURNFTS-vSKddI9G6CQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img3:"./imagens produtos/a-realistic-image-of-a-minimalistic-sneaker-with-a-ICsNURNFTS-vSKddI9G6CQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img4:"./imagens produtos/a-realistic-image-of-a-minimalistic-sneaker-with-a-ICsNURNFTS-vSKddI9G6CQ-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"ThunderStep City Vibe", 
+        preco: 249.90, 
+        descricao:"Sinta a vibe da cidade com o ThunderStep City Vibe, o tênis que une estilo streetwear e conforto para o seu dia a dia.",
+        imagemProduto: {
+            img1:"./imagens produtos/a-photo-of-a-white-sneaker-with-black-and-grey-acc-mHtfvQzUQ1exYU3iFJcsVw-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img2:"./imagens produtos/a-photo-of-a-white-sneaker-with-black-and-grey-acc-mHtfvQzUQ1exYU3iFJcsVw-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img3:"./imagens produtos/a-photo-of-a-white-sneaker-with-black-and-grey-acc-mHtfvQzUQ1exYU3iFJcsVw-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img4:"./imagens produtos/a-photo-of-a-white-sneaker-with-black-and-grey-acc-mHtfvQzUQ1exYU3iFJcsVw-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:8,
+        tituloProduto:"ThunderStep Metro Pulse", 
+        preco: 279.90, 
+        descricao:"Ritmo urbano e versatilidade com o ThunderStep Metro Pulse. O tênis streetwear para quem quer ser único nas ruas.",
+        imagemProduto: {
+            img1:"./imagens produtos/a-3d-render-of-a-minimalist-sneaker-design-for-the-pdvSRBHbTWu4vjo6w_cjOA-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img2:"./imagens produtos/a-3d-render-of-a-minimalist-sneaker-design-for-the-pdvSRBHbTWu4vjo6w_cjOA-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img3:"./imagens produtos/a-3d-render-of-a-minimalist-sneaker-design-for-the-pdvSRBHbTWu4vjo6w_cjOA-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+            img4:"./imagens produtos/a-3d-render-of-a-minimalist-sneaker-design-for-the-pdvSRBHbTWu4vjo6w_cjOA-QVkRw3IPQm6lf2NpPv37FA.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:9,
+        tituloProduto:"ThunderStep Thunderbolt", 
+        preco: 199.90, 
+        descricao:"Corra com energia. Com detalhes vibrantes e um design que brilha à noite, o Neon Blast é feito para quem quer se destacar em qualquer ambiente.",
+        imagemProduto: {
+            img1:"imagens produtos/a-simplistic-white-background-image-of-a-pair-of-b-b6Hoe5tLSneGiIOyy8FuRA-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img2:"imagens produtos/a-simplistic-white-background-image-of-a-pair-of-b-b6Hoe5tLSneGiIOyy8FuRA-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img3:"imagens produtos/a-simplistic-white-background-image-of-a-pair-of-b-b6Hoe5tLSneGiIOyy8FuRA-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+            img4:"imagens produtos/a-simplistic-white-background-image-of-a-pair-of-b-b6Hoe5tLSneGiIOyy8FuRA-VA5Nt8gbR9mzYL4rtoFa6w.jpeg",
+
+        },
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Promoções",
+        exibirHome: true
+    },
+]
